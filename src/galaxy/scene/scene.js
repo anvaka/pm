db6d250fc7dce521c8c7e417ec96df9dbcf2efb2
anvaka <anvaka@gmail.com>
2015-06-15 22:36:57 -0700

    Using only one element nearest to ray

--- src/galaxy/scene/scene.js
+++ src/galaxy/scene/scene.js
@@ -32,9 +32,42 @@ function sceneController(container) {
     hitTest.on('over', handleOver);
   }
 
-  function handleOver(indexes) {
-    renderer.highlight(indexes, 0xff0000);
-    api.fire('over', getHighlightModel(indexes));
+  function handleOver(indexes, ray) {
+    var nearestIndex = nearest(30, ray);
+    renderer.highlight(nearestIndex, 0xff0000);
+    api.fire('over', getHighlightModel(nearestIndex));
+
+    function nearest(maxRadius, ray) {
+      if (indexes.length === 0) return indexes;
+
+      indexes.sort(byProximityToRay);
+
+      var candidate = indexes[0];
+      if (getDistance(candidate) < maxRadius) {
+        return [candidate];
+      }
+
+      return [];
+
+      function byProximityToRay(x, y) {
+        var distX = getDistance(x), distY = getDistance(y);
+        return distX - distY;
+      }
+
+      function getDistance(idx) {
+        var x = positions[idx];
+        var y = positions[idx + 1];
+        var z = positions[idx + 2];
+        var dx = ray.direction.x * (x - ray.origin.x);
+        var dy = ray.direction.y * (y - ray.origin.y);
+        var dz = ray.direction.z * (z - ray.origin.z);
+        var directionDistance = dx + dy + dz;
+        var vx = ray.direction.x * directionDistance + ray.origin.x;
+        var vy = ray.direction.y * directionDistance + ray.origin.y;
+        var vz = ray.direction.z * directionDistance + ray.origin.z;
+        return (vx - x) * (vx - x) + (vy - y) * (vy - y) + (vz - z) * (vz - z);
+      }
+    }
   }
 
   function getHighlightModel(indexes) {
