afb3c06b95144a78a277c16d068d54716c25b30d
anvaka <anvaka@gmail.com>
2015-06-17 03:34:42 -0700

    Trying maco

--- src/galaxy/tooltip.jsx
+++ src/galaxy/tooltip.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+var React = require('react');
+module.exports = require('maco')(tooltip);
 
-export default class Tooltip extends React.Component {
-  render() {
-    var tooltipStyle;
-    var name;
-    var inDegree = 0, outDegree = 0;
-    if (this.props.model) {
-      var model = this.props.model;
-      tooltipStyle = {
-        left: model.x,
-        top: model.y - 35,
-        display: 'block'
-      };
+function tooltip(x) {
+  var name, style = {
+    display : 'none'
+  };
+  var inDegree = 0, outDegree = 0;
+
+  x.render = render;
+
+  function render() {
+    updateViewModel(x.props.model);
+
+    return (
+      <div style={style}
+           className='node-hover-tooltip'>
+        <span className='in-degree'>{inDegree} -&gt; </span>
+          {name}
+        <span className='out-degree'> -&gt; {outDegree}</span>
+      </div>
+    );
+  }
+
+  function updateViewModel(model) {
+    if (model) {
       name = model.node.name;
       inDegree = model.node.in;
       outDegree = model.node.out;
-    } else {
-      tooltipStyle = { display : 'none'};
-      name = '';
-    }
 
-    return <div style={tooltipStyle} className='node-hover-tooltip'>
-      <span className='in-degree'>{inDegree} -&gt; </span>
-        {name}
-      <span className='out-degree'> -&gt; {outDegree}</span>
-    </div>
+      style.left = model.x,
+      style.top = model.y - 35,
+      style.display = 'block'
+    } else {
+      name = '';
+      style.display = 'none';
+    }
   }
 }
