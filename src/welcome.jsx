593eb4781f2285f3e234a36576caa9b7cdf0dc2d
anvaka <anvaka@gmail.com>
2015-07-04 23:24:45 -0700

    Removed cpan

--- src/welcome.jsx
+++ src/welcome.jsx
@@ -7,12 +7,11 @@ export default class WelcomePage extends React.Component {
         <h1>Welcome to the Code Galaxies, Commander</h1>
         <h2>Choose your destination:</h2>
         <ul>
-          <li><a href='#/galaxy/npm'>npm</a></li>
+          <li><a href='#/galaxy/npm?cx=-1345&cy=-7006&cz=-6553&lx=0.621700613972938&ly=-0.6458651572907126&lz=0.30983196944957914&lw=0.31678177960626197&l=1'>npm</a></li>
           <li><a href='#/galaxy/bower'>Bower</a></li>
           <li><a href='#/galaxy/composer'>Composer</a></li>
           <li><a href='#/galaxy/rubygems'>RubyGems</a></li>
-          <li><a href='#/galaxy/gosearch'>Go Search</a></li>
-          <li><a href='#/galaxy/cpan'>cpan</a></li>
+          <li><a href='#/galaxy/gosearch?l=1'>Go Search</a></li>
         </ul>
       </div>
     );
