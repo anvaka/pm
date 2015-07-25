bd3d7456ab9a46b87a81027b56d232a553babce5
anvaka <anvaka@gmail.com>
2015-07-25 00:53:39 -0700

    Fixed template for the default node info

--- src/galaxy/nodeDetails/templates/default.jsx
+++ src/galaxy/nodeDetails/templates/default.jsx
@@ -6,26 +6,30 @@ function template(props) {
   var model = props.model;
 
   return (
-    <div className='package-preview container-fluid row'>
+    <div className='container-fluid row'>
       <div className='hidden-xs'>
         <div className='col-xs-6'>
           <h4 title={model.name}>{model.name}</h4>
         </div>
         <div className="col-xs-3">
           <div className="row">
-            <a href='#' className='in-degree'>
-              <h2>{model.inDegree}</h2>
-            </a>
+            <h2 id={model.id} className='in-degree'>{model.inDegree}</h2>
           </div>
           <div className="row small">{model.inDegreeLabel}</div>
         </div>
         <div className="col-xs-3">
           <div className="row">
-            <a href='#' className='out-degree'>
-              <h2>{model.outDegree}</h2>
-            </a>
+            <h2 id={model.id} className='out-degree'>{model.outDegree}</h2>
           </div>
           <div className="row small">{model.outDegreeLabel}</div>
+        </div>
+      </div>
+
+      <div className='visible-xs-block'>
+        <div className='row info-block'>
+          <div className='col-xs-6 no-overflow'><h5>{model.name}</h5></div>
+          <div id={model.id} className='in-degree col-xs-3'>{model.inDegree}</div>
+          <div id={model.id} className='out-degree col-xs-3'>{model.outDegree}</div>
         </div>
       </div>
     </div>
