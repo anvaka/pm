9c719c37c5689e6eff8573b9bec9d657d46b0ca7
anvaka <anvaka@gmail.com>
2016-01-03 08:40:57 -0800

    Added formatting for deps count

--- src/galaxy/store/baseNodeViewModel.js
+++ src/galaxy/store/baseNodeViewModel.js
@@ -1,21 +1,22 @@
 import getGraphSpecificInfo from './graphSepcific/graphSpecificInfo.js';
 import scene from './scene.js';
+import formatNumber from '../utils/formatNumber.js';
 
 export default getBaseNodeViewModel;
 
 function getBaseNodeViewModel(nodeId) {
   var graphName = scene.getGraphName();
   var graphSpecificInfo = getGraphSpecificInfo(graphName);
-  var nodeInfo = scene.getNodeInfo(nodeId)
+  var nodeInfo = scene.getNodeInfo(nodeId);
 
   return {
     name: nodeInfo.name,
     id: nodeInfo.id,
 
-    inDegree: nodeInfo.in,
+    inDegree: formatNumber(nodeInfo.in),
     inDegreeLabel: graphSpecificInfo.getInDegreeLabel(nodeInfo.in),
 
-    outDegree: nodeInfo.out,
+    outDegree: formatNumber(nodeInfo.out),
     outDegreeLabel: graphSpecificInfo.getOutDegreeLabel(nodeInfo.out)
   };
 }
