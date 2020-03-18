9dbb5eaaf9a88e25a92e885fb5f9577270200126
Andriy Kashcha <anvaka@gmail.com>
2020-03-18 08:47:25 -0700

    fixed the deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+rm -rf build
 npm run build
 cd ./build
+git init
 git add .
 git commit -m 'push to gh-pages'
 git push --force git@github.com:anvaka/pm.git master:gh-pages
+cd ../
+git tag `date "+release-%Y%m%d%H%M%S"`
+git push --tags
