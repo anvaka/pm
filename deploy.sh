f8476aa97e832402150d45838324975dbb3ee3d4
anvaka <anvaka@gmail.com>
2023-05-22 18:48:41 -0700

    moved to main branch

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd ./build
 git init
 git add .
 git commit -m 'push to gh-pages'
-git push --force git@github.com:anvaka/pm.git master:gh-pages
+git push --force git@github.com:anvaka/pm.git main:gh-pages
 cd ../
 git tag `date "+release-%Y%m%d%H%M%S"`
 git push --tags
